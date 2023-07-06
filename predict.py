4d584a6215781f4c87aca8945c8a9474b4daad61
Ziwei Liu <ZiweiLiu0908@gmail.com>
2023-07-06 18:02:57 +0200

    Lenet with brevitas

---
+++ predict.py
@@ -0,0 +1,31 @@
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+
+from model import Model
+
+
+def main():
+    transform = transforms.Compose(
+      [transforms.ToTensor()]
+    )
+
+
+    classes = ('0', '1', '2', '3',
+               '4', '5', '6', '7', '8', '9')
+
+    net = Model()
+    net.load_state_dict(torch.load('models/mnist_0.979.pkl'))
+
+    im = Image.open('1.jpg')
+    im = transform(im)  # [C, H, W]
+    im = torch.unsqueeze(im, dim=0)  # [N, C, H, W]
+
+    with torch.no_grad():
+        outputs = net(im)
+        predict = torch.max(outputs, dim=1)[1].numpy()
+    print(classes[int(predict)])
+
+
+if __name__ == '__main__':
+    main()
