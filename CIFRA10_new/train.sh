e5ced23c51a48f0875551dee21750d0b0f1fbe7c
Ziwei Liu <121568298+ZiweiLiu0908@users.noreply.github.com>
2023-07-15 17:54:24 +0200

    Update train.sh

--- CIFRA10_new/train.sh
+++ CIFRA10_new/train.sh
@@ -1,3 +1,21 @@
+dir="./onnx"
+if [ ! -d "$dir" ];then
+mkdir $dir
+echo "创建onnx文件夹成功"
+else
+echo "onnx文件夹已经存在"
+fi
+
+#!/bin/bash
+dir="./statistics"
+if [ ! -d "$dir" ];then
+mkdir $dir
+echo "创建statistics文件夹成功"
+else
+echo "statistics文件夹已经存在"
+fi
+
+
 python train0.py
 python train1.py
 python train2.py
